3054616e1e87b6c2eac3cb8f3e859e2790447f4f
ShMg <32521952+pythshoma228@users.noreply.github.com>
2020-05-11 14:55:18 +0300

    Add files via upload

---
+++ projects/editArray2.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+void push_everywhere(int*& arr, int& size, int value, int element)
+{
+    int* newArray = new int[size + 1];
+    int j = 0;
+
+    for (int i = 0; i <= size; i++)
+    {
+        if (i == element)
+        {
+            newArray[i] = value;
+            j++;
+        }
+        newArray[i + j] = arr[i];
+    }
+    size++;
+    delete[] arr;
+    arr = newArray;
+}
+
+void pop_everywhere(int*& arr, int& size, int element)
+{
+    int* newArray = new int[size - 1];
+    arr[element] = newArray[size];
+    int j = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (i == element)
+        {
+            newArray[i] = arr[i + 1];
+            j++;
+        }
+        newArray[i] = arr[i + j];
+    }
+    size--;
+    delete[] arr;
+    arr = newArray;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int size = 5;
+    int* arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 10; // make new elements
+    }
+    for (int i = 0; i < size; i++) { cout << arr[i] << "\t"; } // show array
+
+    cout << endl;
+    push_everywhere(arr, size, 12, 3); //enter new elements
+    for (int i = 0; i < size; i++) { cout << arr[i] << "\t"; }
+
+    cout << endl;
+    pop_everywhere(arr, size, 3); //delete new elements
+    for (int i = 0; i < size; i++) { cout << arr[i] << "\t"; }
+
+    delete[] arr;
+    return 0;
+
+}
